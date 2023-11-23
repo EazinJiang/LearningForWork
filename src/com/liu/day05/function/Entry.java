8a01fb51ea764a2f19a3ab06c8712fb798c134b1
aireen <51255902005@stu.ecnu.edu.cn>
2023-11-23 21:48:42 +0800

    day5

---
+++ src/com/liu/day05/function/Entry.java
@@ -0,0 +1,27 @@
+package com.liu.day05.function;
+
+import java.util.Random;
+import java.util.function.Function;
+
+public class Entry {
+
+    public static void main(String[] args) {
+
+        // 定义一个存储学生姓名的数组
+        String[] userNames = {"郭靖", "黄药师" , "黄蓉" , "欧阳锋" , "老顽童" , "瑛姑"} ;
+        Random random = new Random();
+
+        // 在等号右边补全代码实现getUserName方法调用
+        String userName = getUserName(strings -> userNames[random.nextInt(userNames.length)], userNames) ;
+
+        // 输出
+        System.out.println("请" + userName + "同学回答问题！");
+
+    }
+
+    // 从数组中随机获取一个元素
+    public static String getUserName(Function<String[] , String> function , String[] userNames) {
+        return function.apply(userNames) ;
+    }
+
+}
